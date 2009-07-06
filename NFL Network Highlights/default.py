8830758015140394ca876c047069fff23b71571f
mdpauley <mdpauley@gmail.com>
2009-07-06 20:45:51 +0000

    updated to use local sqlite db because of the json changes from nfl.com

--- NFL Network Highlights/default.py
+++ NFL Network Highlights/default.py
@@ -2,7 +2,7 @@
 __plugin__ = "NFL Network Highlights"
 __author__ = "MDPauley"
 __url__ = ""
-__version__ = "0.0.6"
+__version__ = "0.7.5"
 
 import urllib, urllib2, re
 import string, os, time, datetime
@@ -10,145 +10,187 @@ import xbmc, xbmcgui, xbmcplugin
 
 xbmcplugin.addSortMethod(int(sys.argv[1]), xbmcplugin.SORT_METHOD_NONE)
 
-def catsInitial():
-	addDir('Featured','http://www.nfl.com/ajax/videos?categoryId=featured', 1 , '')
-	addDir('NFL Network','http://www.nfl.com/ajax/videos?categoryId=featured', 2 , '')
-	addDir('Teams','http://www.nfl.com/ajax/videos?categoryId=featured', 3 , '')
-	addDir('Highlights','http://www.nfl.com/ajax/videos?categoryId=highlights', 50 , '')
-	addDir('NFL Films', 'http://nfl.com/ajax/videos?categoryId=nflFilms', 50, '')
-	addDir('Events', 'http://nfl.com/ajax/videos?categoryId=events', 50, '')
-	addDir('Search', 'search', 25, '')
+# Shared resources
+BASE_RESOURCE_PATH = os.path.join( os.getcwd(), "resources" )
+# append the proper platforms folder to our path, xbox is the same as win32
+env = ( os.environ.get( "OS", "win32" ), "win32", )[ os.environ.get( "OS", "win32" ) == "xbox" ]
+sys.path.append( os.path.join( BASE_RESOURCE_PATH, "platform_libraries", env ) )
+#load sqlite library
+from pysqlite2 import dbapi2 as sqlite
+
+#Does nfl.db exsist?
+nfl_db_path = os.path.join( os.getcwd(), "nfl.db" )
+
+# Create nfl.db if needed
+#if ( not os.path.isfile( nfl_db_path ) ):
 	
-def catsFeatured():
-	addDir('Featured Videos','http://www.nfl.com/ajax/videos?categoryId=featured', 50 , '')
-	addDir('Video Picks of the Day','http://www.nfl.com/ajax/videos?categoryId=featured&filter=day-picks', 50 , '')
-	addDir('Game Previews','http://www.nfl.com/ajax/videos?categoryId=featured&filter=previews', 50 , '')
-	addDir('Can\'t-Miss Plays','http://www.nfl.com/ajax/videos?categoryId=featured&filter=cant-miss-plays', 50 , '')
-	addDir('Rookie of the Week','http://www.nfl.com/ajax/videos?categoryId=featured&filter=rookies', 50 , '')
-	addDir('Defensive Player of the Week','http://www.nfl.com/ajax/videos?categoryId=featured&filter=players-defense', 50 , '')
-	addDir('Air & Ground Players of the Week','http://www.nfl.com/ajax/videos?categoryId=featured&filter=players-air-and-ground', 50 , '')
-	addDir('Coach of the Week','http://www.nfl.com/ajax/videos?categoryId=featured&filter=coaches', 50 , '')
+conn = sqlite.connect(nfl_db_path)
+c = conn.cursor()
+try:
+	c.execute('create table nfl_videos (videoCMSID text, channelId text, headline text, pageURL text, caption text, runtime text, smallImage text)')
+except conn.OperationalError, msg:
+	print msg
 
-def catsNFLNetwork():
-	addDir('NFL Total Access','http://www.nfl.com/ajax/videos?categoryId=nflNetwork&filter=nfl-total-access', 50 , 'http://www.nfl.com/img/video/logos/total_access.gif')
-	addDir('Team Cam','http://www.nfl.com/ajax/videos?categoryId=nflNetwork&filter=team-cam', 50 , 'http://www.nfl.com/img/video/logos/team_cam.gif')
-	addDir('NFL GameDay','http://www.nfl.com/ajax/videos?categoryId=nflNetwork&filter=nfl-gameday', 50 , 'http://www.nfl.com/img/video/logos/gameday.gif')
-	addDir('NFL Replay','http://www.nfl.com/ajax/videos?categoryId=nflNetwork&filter=nfl-replay', 50 , 'http://www.nfl.com/img/video/logos/replay.gif')
-	addDir('Playbook','http://www.nfl.com/ajax/videos?categoryId=nflNetwork&filter=playbook', 50 , 'http://www.nfl.com/img/video/logos/playbook.gif')
-	addDir('College Football Now','http://www.nfl.com/ajax/videos?categoryId=nflNetwork&filter=college-daily', 50 , 'http://www.nfl.com/img/video/logos/college_scoreboard.gif')
-	addDir('NFL Top Ten','http://www.nfl.com/ajax/videos?categoryId=nflNetwork&filter=nfl-top-10', 50 , 'http://www.nfl.com/img/video/logos/top_ten.gif')
-	addDir('Inside Training Camp','http://www.nfl.com/ajax/videos?categoryId=nflNetwork&filter=in-training-camp', 50 , 'http://www.nfl.com/img/video/logos/training_camp.gif')
-	addDir('Path to the Draft','http://www.nfl.com/ajax/videos?categoryId=nflNetwork&filter=path-draft', 50 , 'http://www.nfl.com/img/video/logos/path_draft.gif')
 
-def mkTeamDir():
-        req = urllib2.Request('http://www.nfl.com/videos')
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')
-        response = urllib2.urlopen(req)
-        link=response.read()	
-        code=re.sub('\r','',link)
-        code=re.sub('\n',' ',code)
-        code=re.sub('\t',' ',code)
-        code=re.sub('  ','',code)
-        response.close()
-	code=code.split('<img src=\"http://static.nfl.com/static/content//public/image/videos/navigation/teams.png\" alt=\"\" /></div><div class=\"channels\">')
-	code=code[1].split('</ul></div>')
-	p=re.compile('<li><a href=\"(.+?)\">(.+?)</a></li> ')
-	match=p.findall(code[0])
-        for teamcode, teamname in match:
-                addDir(teamname,'http://www.nfl.com' + teamcode, 60 , '')	
-        
-"""
-	INDEX()
-	Parses the data to create the filelist
-"""
+def catInitial():
+	addDir('Shows','http://www.nfl.com/ajax/videos?categoryId=featured', 10 , '')
+	addDir('Teams','http://www.nfl.com/ajax/videos?categoryId=featured', 20 , '')
+	addDir('Spotlight', 'http://nfl.com/ajax/videos?categoryId=nflFilms', 40, '')
+	addDir('Events', 'http://nfl.com/ajax/videos?categoryId=events', 50, '')
+
+def catShows():
+	req = urllib2.Request('http://www.nfl.com/videos')
+	req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')
+	response = urllib2.urlopen(req)
+	link=response.read()	
+	code=re.sub('\r','',link)
+	code=re.sub('\n',' ',code)
+	code=re.sub('\t',' ',code)
+	code=re.sub('  ','',code)
+	code=re.sub('&#039;','\'',code)
+	response.close()
+	code=code.split('<li id=\"alt-nv-group-shows\" class=\"alt-nv-group\">');
+	code=code[1].split('<li id=\"alt-nv-group-teams\" class=\"alt-nv-group\">');
+	code=re.sub('<h3><a href=\"#\">Shows</a></h3> <div class=\"channel-content\"><div class=\"alt-nv-content-wrapper\"> <div class=\"video-nv-content bottom\"> <img src=\"http://static.nfl.com/static/content//public/image/videos/navigation/shows.png\" alt=\"\" /></div><div class=\"channels\"><ul>', '', code[0])
+	p=re.compile('<li><a href=\"(.+?)\">(.+?)</a></li>')
+	match=p.findall(code)
+        for url, name in match:
+        	url = re.sub('/videos/','',url)
+                addDir(name, url, 500 , '')	
+
+def catTeams():
+	req = urllib2.Request('http://www.nfl.com/videos')
+	req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')
+	response = urllib2.urlopen(req)
+	link=response.read()	
+	code=re.sub('\r','',link)
+	code=re.sub('\n',' ',code)
+	code=re.sub('\t',' ',code)
+	code=re.sub('  ','',code)
+	code=re.sub('&#039;','\'',code)
+	response.close()
+	code=code.split('<li id=\"alt-nv-group-teams\" class=\"alt-nv-group\">');
+	code=code[1].split('<li id=\"alt-nv-group-game-highlights\" class=\"alt-nv-group\">');
+	code=re.sub('<h3><a href=\"#\">Shows</a></h3> <div class=\"channel-content\"><div class=\"alt-nv-content-wrapper\"> <div class=\"video-nv-content bottom\"> <img src=\"http://static.nfl.com/static/content//public/image/videos/navigation/shows.png\" alt=\"\" /></div><div class=\"channels\"><ul>', '', code[0])
+	p=re.compile('<li><a href=\"(.+?)\">(.+?)</a></li>')
+	match=p.findall(code)
+        for url, name in match:
+        	url = re.sub('/videos/','',url)
+                addDir(name, url, 500 , '')
+
+def catHighlights():
+	req = urllib2.Request('http://www.nfl.com/videos')
+	req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')
+	response = urllib2.urlopen(req)
+	link=response.read()	
+	code=re.sub('\r','',link)
+	code=re.sub('\n',' ',code)
+	code=re.sub('\t',' ',code)
+	code=re.sub('  ','',code)
+	code=re.sub('&#039;','\'',code)
+	response.close()
+	code=code.split('<li id=\"alt-nv-group-game-highlights\" class=\"alt-nv-group\">');
+	code=code[1].split('<li id=\"alt-nv-group-spotlight\" class=\"alt-nv-group\">');
+	code=re.sub('<h3><a href=\"#\">Shows</a></h3> <div class=\"channel-content\"><div class=\"alt-nv-content-wrapper\"> <div class=\"video-nv-content bottom\"> <img src=\"http://static.nfl.com/static/content//public/image/videos/navigation/shows.png\" alt=\"\" /></div><div class=\"channels\"><ul>', '', code[0])
+	p=re.compile('<li><a href=\"(.+?)\">(.+?)</a></li>')
+	match=p.findall(code)
+        for url, name in match:
+        	url = re.sub('/videos/','',url)
+                addDir(name, url, 500 , '')
+
+def catSpotlight():
+	req = urllib2.Request('http://www.nfl.com/videos')
+	req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')
+	response = urllib2.urlopen(req)
+	link=response.read()	
+	code=re.sub('\r','',link)
+	code=re.sub('\n',' ',code)
+	code=re.sub('\t',' ',code)
+	code=re.sub('  ','',code)
+	code=re.sub('&#039;','\'',code)
+	response.close()
+	code=code.split('<li id=\"alt-nv-group-spotlight\" class=\"alt-nv-group\">');
+	code=code[1].split('<li id=\"alt-nv-group-events\" class=\"alt-nv-group\">');
+	code=re.sub('<h3><a href=\"#\">Shows</a></h3> <div class=\"channel-content\"><div class=\"alt-nv-content-wrapper\"> <div class=\"video-nv-content bottom\"> <img src=\"http://static.nfl.com/static/content//public/image/videos/navigation/shows.png\" alt=\"\" /></div><div class=\"channels\"><ul>', '', code[0])
+	p=re.compile('<li><a href=\"(.+?)\">(.+?)</a></li>')
+	match=p.findall(code)
+        for url, name in match:
+        	url = re.sub('/videos/','',url)
+                addDir(name, url, 500 , '')
+
+def catEvents():
+	req = urllib2.Request('http://www.nfl.com/videos')
+	req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')
+	response = urllib2.urlopen(req)
+	link=response.read()	
+	code=re.sub('\r','',link)
+	code=re.sub('\n',' ',code)
+	code=re.sub('\t',' ',code)
+	code=re.sub('  ','',code)
+	code=re.sub('&#039;','\'',code)
+	response.close()
+	code=code.split('<li id=\"alt-nv-group-events\" class=\"alt-nv-group\">');
+	code=code[1].split('<li id=\"alt-nv-group-all-videos\" class=\"alt-nv-group\">');
+	code=re.sub('<h3><a href=\"#\">Shows</a></h3> <div class=\"channel-content\"><div class=\"alt-nv-content-wrapper\"> <div class=\"video-nv-content bottom\"> <img src=\"http://static.nfl.com/static/content//public/image/videos/navigation/shows.png\" alt=\"\" /></div><div class=\"channels\"><ul>', '', code[0])
+	p=re.compile('<li><a href=\"(.+?)\">(.+?)</a></li>')
+	match=p.findall(code)
+        for url, name in match:
+        	url = re.sub('/videos/','',url)
+                addDir(name, url, 500 , '')
+
 def listvideos(data):
 	res=[]
 	#this is where the fun starts, so we'll print some text here to
-	#locate this section of code in the boxee debug logs.
-	print '**listvideos()'		
-        req = urllib2.Request(data)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')
-        response = urllib2.urlopen(req)
-        link=response.read()
-        code=re.sub('&#39;','',link)
-        code2=re.sub('&amp;','&',code)
-        response.close()
-        p=re.compile('\"filePath\":\"(.+?)\",\"fileName\":\"(.+?)\",\"controlPanelImage\":\"(.+?)\",\"mainTitle\":\"(.+?)\".+?\"captionBlurb\":\"(.+?)\".+?\"thumbRunTime\":\"(.+?)\",')
-        match=p.findall(code2)
-        for filePath, fileName, controlPanelImage, mainTitle, captionBlurb, thumbRunTime in match:
-                res.append((filePath, fileName, controlPanelImage, mainTitle, captionBlurb, thumbRunTime))     
-        for filePath, fileName, controlPanelImage, mainTitle, captionBlurb, thumbRunTime in res:
-		videoinfo = {'Title': mainTitle, "Date": "2009-01-01", 'Plot': captionBlurb, 'Genre': 'Sports', 'Duration': thumbRunTime}
-                addLink(filePath + fileName, controlPanelImage, mainTitle, videoinfo)
-
-"""
-	INDEX()
-	Parses the data to create the filelist
-"""
-def listteamvideos(data):
-	res=[]
-	#this is where the fun starts, so we'll print some text here to
-	#locate this section of code in the boxee debug logs.
-	print '**listteamvideos()'
-        req = urllib2.Request(data)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')
-        response = urllib2.urlopen(req)
-        link=response.read()
-        code=re.sub('\r','',link)
-        code=re.sub('\n',' ',code)
-        code=re.sub('\t',' ',code)
-        code=re.sub('  ','',code)
-        response.close()
-        p=re.compile('{ briefHeadline:\"(.+?)\", captionBlurb: \"(.+?)\", date: \"(.+?)\", videoCMSID: \"(.+?)\", runTime:\"(.+?)\", videoDetailUrl: \"(.+?)\", smallImage: \"(.+?)", xSmallImage:\"(.+?)\"}')
-        match=p.findall(code)
-        for briefHeadline, captionBlurb, date, videoCMSID, runTime, videoDetailUrl, smallImage, xSmallImage in match:
-        	res.append((briefHeadline, captionBlurb, date, videoCMSID, runTime, videoDetailUrl, smallImage, xSmallImage))     
-        	req = urllib2.Request('http://www.nfl.com' + videoDetailUrl)
-		req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')
-		response = urllib2.urlopen(req)
-		link2=response.read()
-		code2=re.sub('&#39;','',link2)
-		code2=re.sub('&amp;','&',code2)
-		code2=re.sub('\r','',code2)
-		code2=re.sub('\n',' ',code2)
-		code2=re.sub('\t',' ',code2)
-		code2=re.sub('  ',' ',code2)
-		code2=re.sub('  ',' ',code2)
-		response.close()
-		code2=code2.split('<!-- VIDEO DETAILS:')
-		code2=code2[1].split(' -->.+?<!-- BEGIN PAGE widget/video/carousel -->')
-		p=re.compile('Video URL:  (.+?) Run Time:')
-		match2=p.findall(code2[0])
-		for vURL in match2:
-			videoinfo = {'Title': captionBlurb, "Date": "n/a", 'Plot': briefHeadline, 'Genre': 'Sports', 'Duration': "n/a"}
-			addLink(vURL, smallImage, captionBlurb, videoinfo)  
-
-
-def listQvideos(data):
-	res=[]
-	#this is where the fun starts, so we'll print some text here to
-	#locate this section of code in the boxee debug logs.
 	print '**listvideos()'
 		
-        req = urllib2.Request(data)
+        req = urllib2.Request('http://www.nfl.com/ajax/videos/v2?channelId=' + data)
         req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')
         response = urllib2.urlopen(req)
         link=response.read()
         code=re.sub('&#39;','',link)
         code2=re.sub('&amp;','&',code)
         response.close()
-        p=re.compile('\"filePath\":\"(.+?)\",\"fileName\":\"(.+?)\",\"controlPanelImage\":\"(.+?)\",\"mainTitle\":\"\".+?\"captionBlurb\":\"(.+?)\".+?\"thumbRunTime\":\"(.+?)\",')
+        p=re.compile('\"videoCMSID\":\"(.+?)\".+?,\"smallImage\":\"(.+?)\".+?,\"videoDetailUrl\":\"(.+?)\",')
         match=p.findall(code2)
-        for filePath, fileName, controlPanelImage, captionBlurb, thumbRunTime in match:
-                res.append((filePath, fileName, controlPanelImage, captionBlurb, thumbRunTime))     
-        for filePath, fileName, controlPanelImage, captionBlurb, thumbRunTime in res:
-		videoinfo = {'Title': captionBlurb, "Date": "2009-01-01", 'Plot': captionBlurb, 'Genre': 'Sports', 'Duration': thumbRunTime}
-                addLink(filePath + fileName, controlPanelImage, captionBlurb, videoinfo)
+        for videoCMSID, smallImage, videoDetailUrl in match:   
+		sql = "SELECT * from nfl_videos WHERE videoCMSID = '" + videoCMSID + "'"
+		c.execute(sql)
+		r = c.fetchone()
+		if r == None:	
+			req = urllib2.Request('http://www.nfl.com' + videoDetailUrl)
+			req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')
+			response = urllib2.urlopen(req)
+			link=response.read()	
+			code=re.sub('\r','',link)
+			code=re.sub('\n',' ',code)
+			code=re.sub('\t',' ',code)
+			code=re.sub('  ','',code)
+			code=re.sub('&amp;','&',code)
+			response.close()       
+			code=code.split('<!-- VIDEO DETAILS:')
+			code=code[1].split('-->')
+			p=re.compile('Story Headline:.(.+?)Caption:(.+?)Video.URL:(.+?)Run.Time:..+?\((.+?)\)')
+			match2=p.findall(code[0])
+			for Headline, Caption, VideoURL, RunTime in match2:
+				Headline=re.sub('&amp;','&',Headline)
+				Caption=re.sub('&amp;','&',Caption)
+				res.append((Headline, Caption, VideoURL, RunTime))          	 
+				sql = "insert into nfl_videos values ('" + videoCMSID + "', '" + data + "', '" + re.sub('\'', '', Headline) + "', '" + VideoURL + "', '" + re.sub('\'', '', Caption) + "', '" + RunTime + "', '" + smallImage + "')"
+				c.execute(sql)
+				conn.commit()
+			for Headline, Caption, VideoURL, RunTime in match2:
+				Headline=re.sub('&amp;','&',Headline)
+				Caption=re.sub('&amp;','&',Caption)
+				videoinfo = {'Title': Headline, "Date": "2009-01-01", 'Plot': Caption, 'Genre': 'Sports'}
+				addLink(VideoURL, smallImage, Headline, videoinfo)
+		elif r != None:
+			res.append((r[2], r[4], r[3], r[5]))          	 
+			headline=re.sub('&amp;','&',r[2])
+			caption=re.sub('&amp;','&',r[4])
+			videoinfo = {'Title': headline, "Date": "2009-01-01", 'Plot': caption, 'Genre': 'Sports'}
+			addLink(r[3], r[6], headline, videoinfo)
 
-"""
-	addLink()
-	this function simply adds a media link to boxee's current screen
-"""
+	
 def addLink(url, thumb, name, info):
 	ok=True
 	liz=xbmcgui.ListItem( name, iconImage="DefaultVideo.png", thumbnailImage= thumb )
@@ -156,10 +198,6 @@ def addLink(url, thumb, name, info):
 	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
 	return ok
 
-"""
-	addDir()
-	this function simply adds a directory link to boxee's current screen
-"""
 def addDir(name,url,mode,iconimage, plot=''):
 	u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
 	ok=True
@@ -171,18 +209,6 @@ def addDir(name,url,mode,iconimage, plot=''):
 	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
 	return ok
 
-def Qsearch(title):
-    keyb = xbmc.Keyboard('', title)
-    keyb.doModal()
-    if (keyb.isConfirmed()):
-        term = keyb.getText()
-    	term = re.sub(' ','+',term)
-    	listQvideos('http://nfl.com/ajax/videos?categoryId=find&quickSearch=' + term)    
-
-"""
-	getParams()
-	grab parameters passed by the available functions in this script
-"""
 def getParams():
 	param=[]
 	paramstring=sys.argv[2]
@@ -221,18 +247,21 @@ except:
 #check $mode and execute that mode
 if mode==None or url==None or len(url)<1:
     print "CATEGORY INDEX : "
-    catsInitial()
-elif mode==1:
-    catsFeatured()
-elif mode==2:
-    catsNFLNetwork()
-elif mode==3:
-    mkTeamDir()
-elif mode==25:
-    Qsearch('Search')
+    catInitial()
+elif mode==10:
+    catShows()
+elif mode==20:
+    catTeams()
+elif mode==30:
+    catHighlights()
+elif mode==40:
+    catSpotlight()
 elif mode==50:
+    catEvents()
+elif mode==500:
     listvideos(url)
-elif mode==60:
-    listteamvideos(url)
-#xbmcplugin.addSortMethod(int(sys.argv[1]), xbmcplugin.SORT_METHOD_TITLE)
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+
+c.close()
+	
