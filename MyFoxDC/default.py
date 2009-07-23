627a370f105fb441d75071c99abfc23a9fd4bd8c
mdpauley <mdpauley@gmail.com>
2009-07-23 01:19:34 +0000

    updates and new plugins

--- MyFoxDC/default.py
+++ MyFoxDC/default.py
@@ -1,5 +1,5 @@
 
-__plugin__ = "FoodNetwork"
+__plugin__ = "MyFoxDC Live"
 __author__ = "MDPauley"
 __url__ = ""
 __version__ = "0.7.5"
@@ -11,7 +11,7 @@ import xbmc, xbmcgui, xbmcplugin
 xbmcplugin.addSortMethod(int(sys.argv[1]), xbmcplugin.SORT_METHOD_NONE)
 
 def catsInitial():
-        req = urllib2.Request('http://www.foodnetwork.com/video-library/index.html')
+        req = urllib2.Request('http://www.myfoxdc.com/subindex/live_video/news_now')
         req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')
         response = urllib2.urlopen(req)
         link=response.read()	
@@ -20,45 +20,24 @@ def catsInitial():
         code=re.sub('\t',' ',code)
         code=re.sub('  ','',code)
         response.close()	
-	code=code.split('<h3>Video Channels</h3>')
-	p=re.compile('<h4>\"(.+?)\"*Full Episodes</h4><div id=\"chnl-(.+?)\" class=\"crslFour92 crsl-w\">')
-	match=p.findall(code[1])
-        for showname, showcode in match:
-                showname=re.sub('\"','',showname)
-                addDir(showname,'http://www.foodnetwork.com/food/channel/xml/0,,' + showcode + ',00.xml', 50 , '')	
+	p=re.compile('<iframe src=\'(.+?)\' frameBorder=\'0\'> </iframe>')
+	match=p.findall(code)
+        for url in match:        
+		req = urllib2.Request(url)
+		req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')
+		response = urllib2.urlopen(req)
+		link=response.read()	
+		code=re.sub('\r','',link)
+		code=re.sub('\n',' ',code)
+		code=re.sub('\t',' ',code)
+		code=re.sub('  ','',code)
+		response.close()	
+        	p=re.compile('<param name=\'fileName\' value=\"(.+?\?bkup.+?)\"> <param name=\'animationatStart\' value=\'true\'>')
+        	match2=p.findall(code)
+        	for url2 in match2:
+			videoinfo = {'Title': 'MyFoxDC Live'}
+            		addLink(url2, 'http://media2.myfoxdc.com/graphics/logos_myfoxdc/png24_myfoxdc149x74.png', 'Watch MyFoxDC Live', videoinfo)
 	        
-"""
-	INDEX()
-	Parses the data to create the filelist
-"""
-def listvideos(data):
-	res=[]
-	#this is where the fun starts, so we'll print some text here to
-	#locate this section of code in the boxee debug logs.
-	print '**listvideos()'	
-        req = urllib2.Request(data)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')
-        response = urllib2.urlopen(req)
-        link=response.read()
-        code=re.sub('&#39;','',link)
-        code=re.sub('&amp;','&',code)
-        code=re.sub('\r','',link)
-        code=re.sub('\n',' ',code)
-        code=re.sub('\t',' ',code)
-        code=re.sub('  ','',code)
-        response.close()
-        p=re.compile('<clipName><\!\[CDATA\[(.+?)\]\]></clipName><length>(.+?)</length><sourceNetwork>(.+?)</sourceNetwork><videoUrl><\!\[CDATA\[(.+?)\]\]></videoUrl><thumbnailUrl><\!\[CDATA\[(.+?)\]\]></thumbnailUrl><abstract><\!\[CDATA\[(.+?)\]\]></abstract>')
-        match=p.findall(code)
-        for mainTitle, thumbRunTime, network, filePath, controlPanelImage, captionBlurb in match:
-                filePath=re.sub('http','mmsh',filePath)
-                res.append((filePath+'?MSWMExt=.asf', mainTitle, controlPanelImage, mainTitle, captionBlurb, thumbRunTime))   
-                print filePath
-        for mainTitle, thumbRunTime, network, filePath, controlPanelImage, captionBlurb in match:
-		filePath=re.sub('http','mmsh',filePath)
-		videoinfo = {'Title': mainTitle, "Date": "2009-01-01", 'Plot': captionBlurb, 'Genre': 'Food', 'Duration': thumbRunTime}
-                addLink(filePath+'?MSWMExt=.asf', controlPanelImage, mainTitle, videoinfo)
-                print filePath
-
 """
 	addLink()
 	this function simply adds a media link to boxee's current screen
